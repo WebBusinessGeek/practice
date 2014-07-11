42498c120648324193b3776d177b3ca9d7503898
Kevin Washington <kevw12188@gmail.com>
2014-07-10 20:59:22 -0400

    added env variables

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => getenv('DB_HOST'),
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => 'HK3LqlvvZXwKlY1GmY6g',
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USERNAME'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
